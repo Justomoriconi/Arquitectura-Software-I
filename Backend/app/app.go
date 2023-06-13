a580636c1013f5bb56f5454a608d9b4924a8b3c4
augusto2902 <augustobruno2902@gmail.com>
2023-06-13 00:08:29 -0300

    cors to send cookie from frontend

--- Backend/app/app.go
+++ Backend/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,7 +11,13 @@ var (
 
 func init() {
 	router = gin.Default()
-	
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
+	config.AllowHeaders = []string{"Content-Type", "Authorization"}
+
+	router.Use(cors.Default())
 }
 
 func StartRoute() {
